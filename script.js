2d15e87b94c982dbd4cdcc2ca9b3b24036f3b4bb
Abigail Sokol <97188426+AbiTheFrog@users.noreply.github.com>
2022-01-05 13:58:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*
  * Planets
- *  Made by Abi
+ *   - Made by Abigail Sokol
+ *   - Last updated 5/1/2022 (d/m/y)
 */
 
 // libraries
@@ -13,7 +14,7 @@ const canv = document.getElementById("main");
 const scene = new gx.Scene();
 const cam = new gx.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-cam.position.z = -25;
+cam.position.z = -80;
 cam.position.y = 5;
 
 const renderer = new gx.WebGLRenderer({
@@ -68,7 +69,7 @@ class Planet {
         this.r = rad;
         
         const geometry = new gx.SphereGeometry(rad, res, res);
-        const material = new gx.MeshBasicMaterial({map: texture});
+        const material = new gx.MeshBasicMaterial({map: texture, color: 0xCCCCFF});
         this.shape = new gx.Mesh(geometry, material);
         
         this.shape.position.set(ix, iy, iz);
@@ -96,13 +97,13 @@ class Planet {
     update(){
         // damp. (it helps... somehow)
         if(Math.abs(this.vx) > 0.01){
-            this.vx += (this.vx > 0) ? -0.0008 : 0.0008;
+            this.vx += (this.vx > 0) ? -0.0001 : 0.0001;
         }
         if(Math.abs(this.vy) > 0.01){
-            this.vy += (this.vy > 0) ? -0.0008 : 0.0008;
+            this.vy += (this.vy > 0) ? -0.0001 : 0.0001;
         }
         if(Math.abs(this.vz) > 0.01){
-            this.vz += (this.vz > 0) ? -0.0008 : 0.0008;
+            this.vz += (this.vz > 0) ? -0.0001 : 0.0001;
         }
 
         this.x = this.cx + this.vx;
@@ -115,17 +116,37 @@ class Planet {
 
     dist(ex, ey, ez, r){
         const v = Math.sqrt((this.cx - ex) ** 2 + (this.cy - ey) ** 2 + (this.cz - ez) ** 2);
-        return v;
+        return (v > this.r + r) ? v : -1;
     }
 }
 
 class World {
     planets = [];
 
-    constructor(n = 0){
+    create(n = 0){
+        this.planets = [];
         for(var i = 0; i < n; i++){
-            this.planets.push(new Planet(rand(-20, 20), rand(-20, 20), rand(-20, 20), 20, undefined, 0x8888FF, 1));
+            this.planets.push(new Planet(rand(-40, 40), rand(-40, 40), rand(-40, 40), 20, undefined, 0x8888FF, 1));
         }
+    }
+
+    clear(){
+        for(var i = 0; i < this.planets.length; i++){
+            scene.remove(this.planets[i].shape);
+        }
+        this.planets = [];
+    }
+
+    add(item){
+        if(!item){
+            this.planets.push(new Planet(rand(-40, 40), rand(-40, 40), rand(-40, 40), 20, undefined, 0x8888FF, 1));
+        } else {
+            this.planets.push(item);
+        }
+    }
+
+    constructor(n = 0){
+        this.create(n);
     }
 
     update(){
@@ -136,6 +157,32 @@ class World {
                 var ex = this.planets[j];
                 
                 const r3 = p.dist(ex.cx, ex.cy, ex.cz, ex.r) ** 3;
+
+                if(r3 == -1){
+                    var svx = p.vx, svy = p.vy, svz = p.vz;
+                    const mr = ex.m / (p.m + ex.m);
+                    p.vx = ex.vx * mr;
+                    p.vy = ex.vy * mr;
+                    p.vz = ex.vz * mr;
+                    p.m += ex.m;
+                    
+                    // I know this is not how this works
+                    // however, close enough for now
+                    p.shape.scale.x += ex.shape.scale.x;
+                    p.shape.scale.y += ex.shape.scale.y;
+                    p.shape.scale.z += ex.shape.scale.z;
+                    p.r += ex.r;
+
+                    scene.remove(ex.shape);
+
+                    this.planets.splice(j, 1);
+
+                    p.x = (p.cx + ex.cx) / 2;
+                    p.y = (p.cy + ex.cy) / 2;
+                    p.z = (p.cz + ex.cz) / 2;
+                    
+                    continue;
+                }
 
                 const f = (G * p.m * ex.m) / r3;
 
@@ -159,7 +206,21 @@ class World {
     }
 }
 
-const world = new World(5);
+const world = new World(10);
+
+// reset
+document.onkeydown = (event) => {
+    switch(event.key){
+        case 'r':
+            world.clear();
+            world.create(10);
+            break;
+        
+        case 'a':
+            world.add();
+            break;
+    }
+}
 
 // controls
 const controls = new OrbitControls(cam, renderer.domElement);
